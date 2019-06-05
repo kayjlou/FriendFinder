5c4ab85b23361f2d679086bcdec7ca52ce4d62fe
Katie Lewis <klewi314@gmail.com>
2019-06-04 20:43:39 -0700

    Updated image link to show match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,67 +2,70 @@
 //A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
 //Load friend data
-var friends = require('../data/friends.js');
-var path = require('path');
+var friends = require("../data/friends.js");
+var path = require("path");
 
-//var difference = 0;
+module.exports = function (app) {
 
-  module.exports = function(app){
-  
-    //API get request to display JSON of all friends
-    app.get('/api/friends', function(req,res){
-      res.json(friends);
-    });
+  //API get request to display JSON of all friends
+  app.get("/api/friends", function (req, res) {
+    res.json(friends);
+  });
 
-    //API post 
-    app.post('/api/friends', function(req,res){
+  //API post
+  app.post("/api/friends", function (req, res) {
+    //Push data
 
-      //Push data 
-      //friends.push(req.body);
+    //Preset match data
+    var match = {
+      name: "",
+      photo: "",
+      matchDifference: 1000
+    };
 
-      var match = {
-        name: '',
-        photo: '',
-        matchDifference: 1000
-      };
+    var userInfo = req.body;
+    // var userName = userInfo.name;
+    // var userPhoto = userInfo.photo;
+    var userScores = userInfo.scores;
 
-      var userInfo = req.body;
-      var userName = userInfo.name;
-      var userPhoto = userInfo.photo;
-      var userScores = userInfo.scores;
 
+    // friends.push(userInfo);
+
+
+
+
+    //Loops through the current friends to get scores
+    for (var i = 0; i < friends.length; i++) {
+      console.log("Checking match with:" + friends[i].name);
       var totalDifference = 0;
 
-      
-      //Loops through the current friends to get scores
-      for (var i=0; i<friends.length; i++){
-        console.log(friends[i].name);
-        var totalDifference = 0;
 
-        //Runs through the new scores of user input to compare to friends
-        for (var k=0; k<10; k++){
-          totalDifference += Math.abs(parseInt(userScores[k])- parseInt(friends[i].scores));
+      //Runs through the new scores of user input to compare to friends
+      for (var k = 0; k < 11; k++) {
+        totalDifference += Math.abs(
+          parseInt(userScores[k]) - parseInt(friends[i].scores)
+        );
+        console.log(friends[i].name)
+        console.log("the total difference is: " + totalDifference)
 
-          //If sum of differences is less than differences of the current match
-          if(totalDifference<= match.matchDifference){
-
-            //Reset
-            match.name = friends[i].name;
-            match.photo = friends[i].photo;
-            match.matchDifference = totalDifference;
+        //If sum of differences is less than differences of the current match
+        if (totalDifference <= match.matchDifference) {
+          let match = {
+            name: friends[i].name,
+            photo: friends[i].photo,
+            matchDifference: totalDifference
           }
+          console.log("The new match is" + match)
+
+          res.send(match)
         }
-
-    
-      
       }
+    }
 
-      //Returns the match data
-    
+    //Returns the match data
+    console.log("Pushing info to friends json")
+    friends.push(userInfo);
+    // console.log("The match is" + match);
 
-      friends.push(userInfo);
-      res.json(match);
-    });
-  };
-
-
+  });
+};
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 //Dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 //Create an instance of express app
 var app = express();
@@ -12,15 +11,15 @@ var PORT = process.env.PORT || 8080;
 
 //Link to bodyparser for incoming request
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 //Link to application using path
-require('./app/routing/apiRoutes.js')(app);
-require('./app/routing/htmlRoutes.js')(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
-//Listen on port 
-app.listen(PORT, function(){
-  console.log("Listening on PORT: " + PORT);
-})
+//Listen on port
+app.listen(PORT, function() {
+  console.log("Listening on PORT: http://localhost:" + PORT);
+});
